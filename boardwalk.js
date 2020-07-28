15235bec95e79d11019f4f7fdee27a90014d179c
Glitch (hello-webpage) <none>
2020-07-28 18:32:05 +0000

    📹🗽 Checkpoint ./boardwalk.js:18046363/467:18576123/1135 ./balloon.html:18576123/641 ./.glitch-assets:18046363/361:18576123/757

--- boardwalk.js
+++ boardwalk.js
@@ -1,13 +1,19 @@
-var title;
+/* global
+loadImage, createCanvas, image, textFont, fill, text, width, height, background
+*/
+
+var title, myFont, room;
 
 function preload() {
-  
+//   myFont = loadFont('https://cdn.glitch.com/b075c885-b8f5-4cf7-b604-e0b21585599d%2FInconsolata.zip?v=1595960689205');
+  room = loadImage("https://cdn.glitch.com/b075c885-b8f5-4cf7-b604-e0b21585599d%2FarcadeRoom.jpg?v=1595961020109");
 }
 
 function setup() {
-  myFont = loadFont('assets/HighTide.otf');
-}
-
-function draw() {
-  
+  createCanvas(600, 400);
+  image(room, 0, 0, width, height+50)
+  // textFont(myFont);
+  fill(255);
+  textSize(30);
+  text('THE C\.\A\.\S\.\ ARCADE', width/2-125, height/2);
 }
